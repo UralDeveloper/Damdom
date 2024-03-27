1591a816c640d63ea49f8d4cb51fc52659baf515
Alexandr <80260446+UralDeveloper@users.noreply.github.com>
2024-03-27 16:36:20 +0500

    Delete gulp directory

--- gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) => {
-    app.plugins.browsersync.init({
-        server: {
-            baseDir: `./dist/`
-        },
-        notify: true,
-        port: 3000,
-    });
-}
